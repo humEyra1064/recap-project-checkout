6ecfc2b6b6f55f5aadb02f58cf768f3ba541e515
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-04 12:33:46 +0300

    productlist added

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -9,17 +9,19 @@ import axios from "axios"
 const ProductList = () => {
   const url = process.env.REACT_APP_API_URL;
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
   
 
   const getProducts =async()=>{
     try {
       const {data} = await axios(url)
       setProducts(data)
+      setLoading(false)
     } catch (error) {
       console.log(error)
     }
   }
-
+console.log(products)
 useEffect(() => {
   
 getProducts()
@@ -30,18 +32,24 @@ getProducts()
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-        <p className="text-center text-danger w-100">Loading....</p>
-
-        <>
+        {loading ? <p className="text-center text-danger w-100">Loading....</p>
+         : products.length > 0 ? 
+         <>
           <article id="product-panel" className="col-md-5">
             <ProductCard />
           </article>
           <article className="col-md-5 m-3">
             <CardTotal />
           </article>
-        </>
-
+        </>  
+        : 
         <p className="text-center text-danger w-100">No products data...</p>
+          }
+       
+
+        
+
+        
       </div>
     </div>
   );
