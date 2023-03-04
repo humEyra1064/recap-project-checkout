49227d9c27269d4c1ad9c878f98664bb65b46007
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-04 11:41:49 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <NavLink className="navbar-brand" to="/">Checkout</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <NavLink className="nav-link" to="/new-product">New Product</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <NavLink className="nav-link" to="/product-list">Product List</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <NavLink className="nav-link" to="/about">About</NavLink>
         </li>
+        
       </ul>
     </div>
   </div>
